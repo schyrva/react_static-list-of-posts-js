879db36c92500f99c2e0836ebbd9e660af1ad8d4
Stanislav Chyrva <alkefirko@gmail.com>
2024-11-20 13:34:18 +0200

    Solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,24 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+export const PostInfo = ({ post }) => {
+  if (!post) {
+    return <div>Post data is missing</div>;
+  }
+
+  const { title, body, user, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <h2 className="PostInfo__title">{title}</h2>
+      <p className="PostInfo__body">{body}</p>
+      <UserInfo user={user} />
+      {comments && comments.length > 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <div data-cy="NoCommentsMessage">No comments available</div>
+      )}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,15 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  if (!user) {
+    return <div className="UserInfo">User information is not available</div>;
+  }
+
+  const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
